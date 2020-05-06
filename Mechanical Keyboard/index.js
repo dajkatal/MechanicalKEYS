c71cd86fe0ff0ef88b862f7f980de06ddc050bb7
Daj Katal <dajkatal@gmail.com>
2020-05-06 14:50:39 +0400

    Changed some code and removed node_modules

--- Mechanical Keyboard/index.js
+++ Mechanical Keyboard/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:77ef385b9fa2c632b79a066dbda2755b5350914fc33969151b8ddde04ccb1437
-size 15410
+oid sha256:9877e1979fa33da4d3101ab4959de57c4b0a79ddf78d88ff7363a42932d11244
+size 15416
